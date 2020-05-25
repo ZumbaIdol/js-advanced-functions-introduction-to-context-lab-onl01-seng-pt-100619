8334e1770a5b9b24652a41b5a2b51507b41407ab
ZumbaIdol <david.a.foote@gmail.com>
2020-05-25 13:35:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+let createEmployeeRecord = function(ele) {
+    return {
+        firstName: ele[0],
+        familyName: ele[1],
+        title: ele[2],
+        payPerHour: ele[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
